14fbed965519c2a740d471265cf3b2bc2672e377
HectorBritoDev <hectorbritodev@gmail.com>
2020-04-13 16:47:42 -0400

    basic components created

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import './css/Login.css';
 
 const Login = () => {
     return (
-        <div>
+        <div className="login-modal">
+            <div className="login-modal__title">
+                <h3>Iniciar Sesión</h3>
+            </div>
             <label htmlFor="email">Email</label>
             <input type="text" id="email" />
             <label htmlFor="password">Contraseña</label>
