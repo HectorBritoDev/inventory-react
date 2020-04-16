c9afd251dfcb63a4d4c9f7834732738fb359e16d
HectorBritoDev <hectorbritodev@gmail.com>
2020-04-16 15:24:19 -0400

    added Login and show login modal onclick method

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,34 @@
 import React from 'react';
 import './css/Header.css';
+import Login from './Login';
+
+const showLoginModal = () => {
+    const login_modal = document.querySelector('.login-modal');
+    const backdrop = document.querySelector('.backdrop');
+    login_modal.classList.add('show');
+    backdrop.classList.add('show');
+    document.querySelector('#login-email').focus();
+}
 
 const Header = () => {
     return (
-        <header className="main-header">
-            <div className="main-header__brand">
-                <img src="../images/inventory-logo.png" alt="Logo" className="main-header__brand-logo" />
-            </div>
-            <nav className="main-nav">
-                <ul className="main-nav__items">
-                    <li className="main-nav__item">Login</li>
-                    <li className="main-nav__item">Register</li>
-                </ul>
-            </nav>
-        </header >
+        <div>
+            <header className="main-header">
+                <div className="main-header__brand">
+                    <img src="../images/inventory-logo.png" alt="Logo" className="main-header__brand-logo" />
+                </div>
+                <nav className="main-nav">
+                    <ul className="main-nav__items">
+                        <li className="main-nav__item" onClick={showLoginModal}>Login</li>
+                        <li className="main-nav__item">Register</li>
+                    </ul>
+                </nav>
+            </header >
+            <Login />
+        </div>
+
     )
 }
+
 
 export default Header;
