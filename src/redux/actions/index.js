3faf1460e3f3b7dd11a51013f12c33d3afbd6b64
HectorBritoDev <hectorbritodev@gmail.com>
2020-04-21 15:48:07 -0400

    refactorized loginAction method and added getUser

---
+++ src/redux/actions/index.js
@@ -0,0 +1,26 @@
+import { authConst, notificationConst } from './types';
+import authAPI from '../../apis/authAPI';
+
+
+
+export const getUser = () => (dispatch, getState) => {
+    authAPI.get('/user', getState().auth.token.access_token)
+        .then(response => dispatch({ type: authConst.GET_USER_DATA, payload: response }))
+        .then(() => dispatch({ type: authConst.CLEAN }))
+        .catch(error => dispatch({ type: authConst.GET_USER_DATA, payload: error.message }))
+};
+
+export const loginAction = credentials => (dispatch, getState) => {
+    //Solicitamos el token de acceso
+    return authAPI.post('/login', credentials)
+        .then(response => dispatch({ type: authConst.LOGIN, payload: response }))
+        //Una vez recibido el token se solicitan los datos del usuario usando el token         
+        .then(() => dispatch(getUser()))
+        .catch(error => dispatch({ type: authConst.ERROR, payload: error.message }));
+};
+
+export const logout = () => (dispatch, getState) => {
+    return authAPI.post('/logout')
+        .then(() => dispatch({ type: authConst.LOGOUT }))
+        .catch(error => dispatch({ type: notificationConst.ERROR, payload: error.message }));
+};
