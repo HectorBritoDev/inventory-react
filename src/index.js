2812a99fc456d2fd31d42e6b5bccc22473b599b8
HectorBritoDev <hectorbritodev@gmail.com>
2020-04-21 15:32:18 -0400

    store logic removed

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import reduxThunk from 'redux-thunk';
 import { Provider } from 'react-redux';
-import { persistStore } from 'redux-persist';
 import { PersistGate } from 'redux-persist/integration/react';
-import { createStore, applyMiddleware, compose } from 'redux';
 import App from './components/App';
-import reducers from './reducers';
+import { store, persistor } from './redux/store';
 
 
 //redux-dev-tools
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-//create store
-const store = createStore(reducers, composeEnhancers(applyMiddleware(reduxThunk)));
-const persistor = persistStore(store);
 
 ReactDOM.render(
     < Provider store={store}>
