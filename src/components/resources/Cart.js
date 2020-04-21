3b39a9f83ca42b961aace0b85629f142a96db7b8
HectorBritoDev <hectorbritodev@gmail.com>
2020-04-21 15:54:42 -0400

    Cart initial logic

---
+++ src/components/resources/Cart.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../css/Cart.css';
+
+const Cart = props => {
+    const { title, quantity, measure, description } = props;
+    return (
+        <div className="cart">
+            <div className="cart-body">
+                <div className="cart-title">{title}</div>
+                <div className="cart-content">
+                    <div className="cart-content__quantity">{quantity}</div>
+                    <div className="cart-contert__measure">{measure}</div>
+                    <div className="cart-contert__description">{description}</div>
+                </div>
+            </div>
+
+        </div>
+    );
+
+}
+
+export default Cart;
