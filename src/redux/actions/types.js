ac2e96e313907de8e25fdc2079815fb3bba73c55
HectorBritoDev <hectorbritodev@gmail.com>
2020-04-21 15:49:24 -0400

    const names now objects per reducer to avoid unwanted names collition

---
+++ src/redux/actions/types.js
@@ -0,0 +1,14 @@
+
+export const authConst = {
+    LOGIN: 'LOGIN',
+    LOGOUT: 'LOGOUT',
+    GET_USER_DATA: 'GET_USER_DATA',
+    ERROR: 'AUTH_ERROR',
+    CLEAN: 'AUTH_ERROR_CLEAN'
+}
+
+export const notificationConst = {
+    SUCCESS: 'NOTIFICATION_SUCCESSS',
+    ERROR: 'NOTIFICATION_ERROR',
+    CLEAN: 'NOTIFICATION_CLEAN'
+};
