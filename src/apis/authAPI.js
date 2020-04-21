1b64b8448a06081a86c07d55656a1a4fe596999c
HectorBritoDev <hectorbritodev@gmail.com>
2020-04-21 15:51:11 -0400

    refactorized logic and methods authApi

--- src/apis/authAPI.js
+++ src/apis/authAPI.js
@@ -1,36 +1,47 @@
+import { store } from '../redux/store';
 const baseURL = 'http://inventory.test/api';
 const client_name = "Laravel Password Grant Client";
-const headers = {
-    Accept: "application/json",
-    "Content-Type": "application/json",
+
+// function access_token({ token } = store.getState().auth) {
+//     return (token) ? store.getState().auth.token.access_token : '';
+// }
+
+// const headers = {
+//     Accept: "application/json",
+//     "Content-Type": "application/json",
+// }
+function headers() {
+    const { token } = store.getState().auth;
+    let access_token = (token) ? store.getState().auth.token.access_token : '';
+    return {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: 'Bearer ' + access_token
+    }
 }
 
 const _constructUrl = (url) => {
     return `${baseURL}${url}`;
 }
 
-const get = (url, access_token) => fetch(_constructUrl(url), {
+const get = (url) => fetch(_constructUrl(url), {
     method: 'GET',
-    headers: { ...headers, Authorization: `Bearer ${access_token}` },
-}).then(response => {
+    headers: headers(),
+}).then(handleResponse);
 
-    if (response.ok) {
-        return response.json();
-    } else {
-        throw new Error(Response.statusText);
-    }
-});
-
-const post = (url, formData) => fetch(_constructUrl(url), {
+const post = (url, formData = '') => fetch(_constructUrl(url), {
     method: 'POST',
     body: JSON.stringify({ ...formData, client_name }),
-    headers
-}).then(response => {
+    headers: headers()
+}).then(handleResponse);
+
+function handleResponse(response) {
     if (response.ok) {
         return response.json();
     } else {
         throw new Error(response.statusText);
     }
-});
+
+}
 
 export default { get, post };
