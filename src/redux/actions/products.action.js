1bd6bde9f8ba51ec91045e19ed97c28b7201e5aa
HectorBritoDev <hectorbritodev@gmail.com>
2020-04-22 13:16:41 -0400

    fixed typo on products.action

--- src/redux/actions/products.action.js
+++ src/redux/actions/products.action.js
@@ -2,7 +2,7 @@ import { productConst } from './types';
 import fetchAPI from '../../apis/fetchAPI';
 
 export const getAllProducts = () => (dispatch, getState) => {
-    fetchAPI.get('./products')
+    fetchAPI.get('/products')
         .then(response => dispatch({ type: productConst.GET_ALL_PRODUCTS, payload: response.data }))
         .catch(error => dispatch({ type: productConst.ERROR, payload: error }));
 
