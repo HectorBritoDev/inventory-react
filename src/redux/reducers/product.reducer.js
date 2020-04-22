71ced57e308851708e814c677c8d4d3d4860c61c
HectorBritoDev <hectorbritodev@gmail.com>
2020-04-22 13:17:24 -0400

    renamed property products to all

--- src/redux/reducers/product.reducer.js
+++ src/redux/reducers/product.reducer.js
@@ -1,13 +1,13 @@
 import { productConst } from '../actions/types';
 
 const _INITIAL_STATE = {
-    products: {},
+    all: {},
     error: null
 }
 export default (state = _INITIAL_STATE, action) => {
     switch (action.type) {
         case productConst.GET_ALL_PRODUCTS:
-            return { ...state, product: action.payload }
+            return { ...state, all: action.payload }
         case productConst.ERROR:
             return { ...state, error: action.payload }
         case productConst.CLEAN:
