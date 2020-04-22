98f662006cb723fa8267b77b6d6a02970c68c7ec
HectorBritoDev <hectorbritodev@gmail.com>
2020-04-21 22:57:45 -0400

    added html for product section

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,11 +4,11 @@ import Cart from './resources/Cart';
 import './css/Dashboard.css';
 class Dashboard extends React.Component {
 
-    renderTopSection = () => {
+    renderActivitySection = () => {
         return (
-            <div className="dashboard-top">
-                <div className="dashboard-top__header">Actividad</div>
-                <div className="dashboard-top__items">
+            <div className="dashboard-activity">
+                <div className="dashboard-header">Actividad</div>
+                <div className="dashboard-activity__items">
                     <Cart quantity="500" measure="unidades" description="vendidas" />
                     <Cart quantity="500" measure="unidades" description=" vendidas" />
                     <Cart quantity="500" measure="unidades" description=" vendidas" />
@@ -16,13 +16,46 @@ class Dashboard extends React.Component {
                 </div>
             </div>
         );
+    }
+    renderProductSection = () => {
+        return (
+            <div className="dashboard-product">
+                <div className="dashboard-header">Productos</div>
+                <div className="dashboard-product__stadistics">
+                    <div className="dashboard-product__stadistics--details">
+                        <table className="dashboard-product__stadistics--details__table">
+                            <caption className="dashboard-product__stadistics--details__header">Estadisticas</caption>
 
+                            <tbody>
+                                <tr>
+                                    <td >Bajo stock</td>
+                                    <td className="td">3</td>
+                                </tr>
+                                <tr>
+                                    <td >Categorias</td>
+                                    <td className="td">5</td>
+                                </tr>
+                                <tr>
+                                    <td >Productos en categorias</td>
+                                    <td className="td">150</td>
+                                </tr>
+                                <tr>
+                                    <td >Productos sin clasificar</td>
+                                    <td className="td">30</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        );
     }
     render() {
         return (
             <div className="content">
                 <div className="dashboard">
-                    {this.renderTopSection()}
+                    {this.renderActivitySection()}
+                    {this.renderProductSection()}
                 </div>
             </div>
         );
