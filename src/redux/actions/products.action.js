85ed937a0b814cd723ad1975504edc8db5215dbf
HectorBritoDev <hectorbritodev@gmail.com>
2020-04-22 12:56:58 -0400

    getAllProducts action

---
+++ src/redux/actions/products.action.js
@@ -0,0 +1,9 @@
+import { productConst } from './types';
+import fetchAPI from '../../apis/fetchAPI';
+
+export const getAllProducts = () => (dispatch, getState) => {
+    fetchAPI.get('./products')
+        .then(response => dispatch({ type: productConst.GET_ALL_PRODUCTS, payload: response.data }))
+        .catch(error => dispatch({ type: productConst.ERROR, payload: error }));
+
+}
