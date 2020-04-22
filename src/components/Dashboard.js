effd9fefcc0056a8645fe02f6a92aa753a91d565
HectorBritoDev <hectorbritodev@gmail.com>
2020-04-21 21:22:07 -0400

    Dashboard top section html

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,15 +1,28 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Cart from './resources/Cart';
+import './css/Dashboard.css';
 class Dashboard extends React.Component {
 
+    renderTopSection = () => {
+        return (
+            <div className="dashboard-top">
+                <div className="dashboard-top__header">Actividad</div>
+                <div className="dashboard-top__items">
+                    <Cart quantity="500" measure="unidades" description="vendidas" />
+                    <Cart quantity="500" measure="unidades" description=" vendidas" />
+                    <Cart quantity="500" measure="unidades" description=" vendidas" />
+                    <Cart quantity="500" measure="unidades" description=" vendidas" />
+                </div>
+            </div>
+        );
+
+    }
     render() {
         return (
             <div className="content">
                 <div className="dashboard">
-                    <div className="dashboard-top">
-                        <Cart />
-                    </div>
+                    {this.renderTopSection()}
                 </div>
             </div>
         );
