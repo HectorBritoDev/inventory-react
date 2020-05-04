63c6b349bda30ef27d2d259860cbc6c9734ff9ba
HectorBritoDev <hectorbritodev@gmail.com>
2020-05-04 18:57:28 -0400

    created Product Component and added initial logic

---
+++ src/components/Product.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { getAllProducts } from '../redux/actions/products.action';
+
+import './css/Product.scss';
+export class Product extends Component {
+    componentDidMount() {
+        this.props.getAllProducts();
+    }
+    renderTable = () => {
+        var { products } = this.props;
+        if (products.length === 0) {
+            return <div>Cargando...</div>
+        }
+        return (
+            <table className="table">
+                <thead className="table__thead">
+                    <tr>
+                        <th>Nombre</th>
+                        <th>En almacén</th>
+                        <th>Precio unitario</th>
+                        <th>Precio mayoritario</th>
+                    </tr>
+                </thead>
+                <tbody className="table__tbody">
+                    <tr>
+                        <td>Producto 1</td>
+                        <td>500</td>
+                        <td>5.000</td>
+                        <td>4.000</td>
+                    </tr>
+                </tbody>
+            </table>
+        );
+    }
+    render() {
+        return (
+            <div className="product">
+                {this.renderTable()}
+            </div>
+        )
+    }
+}
+const mapStateToProps = state => { return { products: Object.values(state.products) } };
+export default connect(mapStateToProps, { getAllProducts })(Product)
