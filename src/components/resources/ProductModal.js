a8c0a264fc1c785ff1e7bf1e76c8adb52dfd3758
HectorBritoDev <hectorbritodev@gmail.com>
2020-05-04 19:50:48 -0400

    added initial modal div structures

--- src/components/resources/ProductModal.js
+++ src/components/resources/ProductModal.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import { Field, reduxForm } from 'redux-form';
+import '../css/ProductModal.scss';
 
 export class ProductModal extends Component {
     render() {
         return ReactDOM.createPortal(
-            <div>
-                New Product Modal
+            <div className="modal-container">
+                <div className="modal-background" onClick={() => { return console.log('clicked on background') }}></div>
+                <div className="modal-body" onClick={() => { return console.log('clicked on body') }}>
+                    <div className="modal-title">Product</div>
+                    <div className="modal-content">Description</div>
+                </div>
             </div>,
             document.querySelector('#product-modal'));
     }
