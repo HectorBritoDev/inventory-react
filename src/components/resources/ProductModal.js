54f61c15996ef20553bcdfd435f4381ca1a8a05e
HectorBritoDev <hectorbritodev@gmail.com>
2020-05-04 19:03:20 -0400

    product Modal created

---
+++ src/components/resources/ProductModal.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Input } from 'redux-form';
+
+export class ProductModal extends Component {
+    render() {
+        return (
+            <div>
+                New Product Modal
+            </div>
+        )
+    }
+}
+
+export default ProductModal
+
