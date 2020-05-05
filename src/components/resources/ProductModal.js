16d829f23da42a4f14cab28b6e99076f16a7d2e2
HectorBritoDev <hectorbritodev@gmail.com>
2020-05-04 22:55:17 -0400

    added fields to form

--- src/components/resources/ProductModal.js
+++ src/components/resources/ProductModal.js
@@ -14,13 +14,20 @@ export class ProductModal extends Component {
 
     render() {
         return ReactDOM.createPortal(
-            <div className="modal-container" >
+            <div className="modal" >
                 <div className="modal-background" onClick={() => { return console.log('clicked on background') }}></div>
                 <div className="modal-body" onClick={() => { return console.log('clicked on body') }}>
                     <div className="modal-title">Product</div>
                     <div className="modal-content">
                         <form>
-                            <Field name="name" id="name" type="text" placeholder="Nombre" component={this.renderInput} applyClass="modal-input" autoFocus="true" />
+                            <Field name="code" id="code" type="text" placeholder="Código" component={this.renderInput} applyClass="modal-input" autoFocus="true" />
+                            <Field name="name" id="name" type="text" placeholder="Nombre" component={this.renderInput} applyClass="modal-input" />
+                            <Field name="quantity" id="quantity" type="text" placeholder="Cantidad" component={this.renderInput} applyClass="modal-input" />
+                            <Field name="unitary_price" id="unitary_price" type="text" placeholder="Precio Unitario" component={this.renderInput} applyClass="modal-input" />
+                            <Field name="mayotary_price" id="mayotary_price" type="text" placeholder="Precio Mayoritario" component={this.renderInput} applyClass="modal-input" />
+                            <Field name="apply_mayoritary_price_sice" id="apply_mayoritary_price_sice" type="text" placeholder="Aplicar precio mayoritario a partir de" component={this.renderInput} applyClass="modal-input" />
+                            <Field name="category" id="category" type="text" placeholder="Category" component={this.renderInput} applyClass="modal-input" />
+                            <button type="submit" className="modal-submit-button">Agregar</button>
                         </form>
                     </div>
                 </div>
