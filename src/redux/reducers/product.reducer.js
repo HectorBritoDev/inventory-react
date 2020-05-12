7590a097f161fc8bb17f5705e75114a835236f3a
HectorBritoDev <hectorbritodev@gmail.com>
2020-05-12 14:49:59 -0400

    added StoreProduct  action

--- src/redux/reducers/product.reducer.js
+++ src/redux/reducers/product.reducer.js
@@ -6,6 +6,8 @@ export default (state = {}, action) => {
     switch (action.type) {
         case productConst.GET_ALL_PRODUCTS:
             return { ...state, ..._.mapKeys(action.payload, 'id') }
+        case productConst.STORE_PRODUCT:
+            return { ...state, [action.payload.id]: action.payload }
         default:
             return state;
     }
