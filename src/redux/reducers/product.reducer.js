c1e17cf8a172ca6f65542f828ae04966e09aedaf
HectorBritoDev <hectorbritodev@gmail.com>
2020-05-29 22:15:29 -0400

    changed const names in productReducer

--- src/redux/reducers/product.reducer.js
+++ src/redux/reducers/product.reducer.js
@@ -4,13 +4,13 @@ import _ from 'lodash';
 
 export default (state = {}, action) => {
     switch (action.type) {
-        case productConst.GET_ALL_PRODUCTS:
+        case productConst.GET_ALL:
             return { ...state, ..._.mapKeys(action.payload, 'id') }
-        case productConst.STORE_PRODUCT:
+        case productConst.STORE:
             return { ...state, [action.payload.id]: action.payload }
-        case productConst.UPDATE_PRODUCT:
+        case productConst.UPDATE:
             return { ...state, [action.payload.id]: action.payload }
-        case productConst.DESTROY_PRODUCT:
+        case productConst.DESTROY:
             return _.omit(state, action.payload)
         default:
             return state;
